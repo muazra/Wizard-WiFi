2f80b681444d7e0e7b798dd7158f326d66795e66
Muaz Rahman <muazra@gmail.com>
2014-10-12 15:41:23 -0400

    service status shared prefs and icon logic

--- app/src/main/java/com/android/wizard_wifi/MainActivity.java
+++ app/src/main/java/com/android/wizard_wifi/MainActivity.java
@@ -1,35 +1,59 @@
 package com.android.wizard_wifi;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    private SharedPreferences mService;
+    private Boolean mServiceStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        mService = getSharedPreferences("SERVICE", 0);
+        mServiceStatus = mService.getBoolean("service", false);
+
+        MenuItem serviceItem = menu.findItem(R.id.action_service);
+        if(!mServiceStatus)
+            serviceItem.setIcon(R.drawable.ic_action_service_off);
+        else
+            serviceItem.setIcon(R.drawable.ic_action_service_on);
+
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == R.id.action_service) {
+
+            SharedPreferences.Editor editor;
+            editor = mService.edit();
+
+            if(!mServiceStatus) {
+                item.setIcon(R.drawable.ic_action_service_on);
+                editor.putBoolean("service", true);
+                Toast.makeText(this, "Service turned ON", Toast.LENGTH_LONG).show();
+            }
+            else{
+                item.setIcon(R.drawable.ic_action_service_off);
+                editor.putBoolean("service", false);
+                Toast.makeText(this, "Service turned OFF", Toast.LENGTH_LONG).show();
+            }
+
+            editor.apply();
         }
         return super.onOptionsItemSelected(item);
     }
